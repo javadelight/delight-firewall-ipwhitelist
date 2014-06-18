400894e27dd864ceacea225fe622f36c40601852
mxro <mxro@nowhere.com>
2014-06-18 14:44:13 +1200

    Exploring integrating a firewall to http sever Autocommit Wed Jun 18 14:44:13 NZST 2014

--- src/main/java/de/mxro/firewall/ipwhitelist/internal/IPWhitelistRuleImpl.java
+++ src/main/java/de/mxro/firewall/ipwhitelist/internal/IPWhitelistRuleImpl.java
@@ -4,13 +4,24 @@ import de.mxro.abstracthttpfirewall.CheckCallback;
 import de.mxro.abstracthttpfirewall.Rule;
 import de.mxro.abstracthttpserver.Request;
 import de.mxro.abstracthttpserver.Response;
+import de.mxro.firewall.ipwhitelist.IPWhitelist;
 
 public class IPWhitelistRuleImpl implements Rule {
 
+	private final IPWhitelist whitelist;
+	private final Response deniedResponse;
+	
 	@Override
 	public void apply(Request request, Response response, CheckCallback callback) {
-		// TODO Auto-generated method stub
 		
 	}
 
+	public IPWhitelistRuleImpl(IPWhitelist whitelist, Response deniedResponse) {
+		super();
+		this.whitelist = whitelist;
+		this.deniedResponse = deniedResponse;
+	}
+
+	
+	
 }
