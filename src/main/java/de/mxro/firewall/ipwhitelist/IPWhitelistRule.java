f410850b972b222777101ab888d8cf80e2120d38
mxro <mxro@nowhere.com>
2014-06-19 11:14:38 +1200

    Exploring integrating a firewall to http sever Autocommit Thu Jun 19 11:14:38 NZST 2014

--- src/main/java/de/mxro/firewall/ipwhitelist/IPWhitelistRule.java
+++ src/main/java/de/mxro/firewall/ipwhitelist/IPWhitelistRule.java
@@ -1,15 +1,13 @@
 package de.mxro.firewall.ipwhitelist;
 
-import java.util.ArrayList;
-
 import de.mxro.abstracthttpserver.Response;
 import de.mxro.firewall.Rule;
 import de.mxro.firewall.ipwhitelist.internal.IPWhitelistRuleImpl;
 
 public class IPWhitelistRule {
 
-	public static Rule createIPWhitelistRule(Response deniedResponse, ArrayList<String> validTokens, IPWhitelist whitelistService) {
-		return new IPWhitelistRuleImpl(whitelistService, validTokens,deniedResponse);	
+	public static Rule createIPWhitelistRule(Response deniedResponse, IPWhitelist whitelistService) {
+		return new IPWhitelistRuleImpl(whitelistService, deniedResponse);	
 	}
 	
 }
