3d07f66b904d718996325a01b6003940f7a1a37b
mxro <mxro@nowhere.com>
2014-06-18 14:51:13 +1200

    Exploring integrating a firewall to http sever Autocommit Wed Jun 18 14:51:13 NZST 2014

--- src/main/java/de/mxro/firewall/ipwhitelist/internal/IPWhitelistRuleImpl.java
+++ src/main/java/de/mxro/firewall/ipwhitelist/internal/IPWhitelistRuleImpl.java
@@ -1,5 +1,6 @@
 package de.mxro.firewall.ipwhitelist.internal;
 
+import io.nextweb.fn.callbacks.ValueCallback;
 import de.mxro.abstracthttpfirewall.CheckCallback;
 import de.mxro.abstracthttpfirewall.Rule;
 import de.mxro.abstracthttpserver.Request;
@@ -14,7 +15,25 @@ public class IPWhitelistRuleImpl implements Rule {
 	@Override
 	public void apply(final Request request, final Response response, final CheckCallback callback) {
 		
-		
+		whitelist.checkIP(request.getSourceAddress().getAddress(), new ValueCallback<Boolean>() {
+			
+			@Override
+			public void onSuccess(Boolean value) {
+				if (!value) {
+					response.setAll(deniedResponse);
+					callback.block();
+					return;
+				}
+				
+				callback.proceed();
+				
+			}
+			
+			@Override
+			public void onFailure(Throwable t) {
+				callback.onFailure(t);
+			}
+		});
 		
 	}
 
