7b6aff06130f5fa08f81a2bf48fba0155072cb7a
mxro <mxro@nowhere.com>
2015-01-15 08:10:18 +1100

    Linking logs, properties and metrics to server Autocommit Thu Jan 15 08:10:18 EST 2015

--- src/main/java/de/mxro/firewall/ipwhitelist/internal/IPWhitelistRuleImpl.java
+++ src/main/java/de/mxro/firewall/ipwhitelist/internal/IPWhitelistRuleImpl.java
@@ -6,42 +6,47 @@ import de.mxro.firewall.Rule;
 import de.mxro.firewall.ipwhitelist.IPWhitelist;
 import de.mxro.httpserver.Request;
 import de.mxro.httpserver.Response;
+import de.mxro.server.ComponentContext;
 
 public class IPWhitelistRuleImpl implements Rule {
 
-	private final IPWhitelist whitelist;
-	private final Response deniedResponse;
-	
-	@Override
-	public void apply(final Request request, final Response response, final CheckCallback callback) {
-		
-		whitelist.ipInWhitelist(request.getSourceAddress().getAddress(), new ValueCallback<Boolean>() {
-			
-			@Override
-			public void onSuccess(Boolean value) {
-				if (!value) {
-					response.setAll(deniedResponse);
-					callback.block();
-					return;
-				}
-				
-				callback.allow();
-			}
-			
-			@Override
-			public void onFailure(Throwable t) {
-				callback.onFailure(t);
-			}
-		});
-		
-	}
+    private final IPWhitelist whitelist;
 
-	public IPWhitelistRuleImpl(IPWhitelist whitelist, Response deniedResponse) {
-		super();
-		this.whitelist = whitelist;
-		this.deniedResponse = deniedResponse;
-	}
+    private final String whitelistProperty;
+    private final ComponentContext ctx;
 
-	
-	
+    private final Response deniedResponse;
+
+    @Override
+    public void apply(final Request request, final Response response, final CheckCallback callback) {
+
+        whitelist.ipInWhitelist(request.getSourceAddress().getAddress(), new ValueCallback<Boolean>() {
+
+            @Override
+            public void onSuccess(final Boolean value) {
+                if (!value) {
+                    response.setAll(deniedResponse);
+                    callback.block();
+                    return;
+                }
+
+                callback.allow();
+            }
+
+            @Override
+            public void onFailure(final Throwable t) {
+                callback.onFailure(t);
+            }
+        });
+
+    }
+
+    public IPWhitelistRuleImpl(final String whitelistProperty, final ComponentContext ctx, final Response deniedResponse) {
+        super();
+        this.whitelist = null;
+        this.deniedResponse = deniedResponse;
+        this.whitelistProperty = whitelistProperty;
+        this.ctx = ctx;
+    }
+
 }
